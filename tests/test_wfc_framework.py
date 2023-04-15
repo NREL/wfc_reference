4bc961bcd56cf2cbe358b8bdfe0a30d4b504b60e
Paul <paul.fleming@nrel.gov>
2023-04-14 21:09:35 -0600

    Add test folder

---
+++ tests/test_wfc_framework.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `wfc_reference` package."""
+
+
+import unittest
+
+from wfc_reference import wfc_reference
+
+
+class TestWfc_reference(unittest.TestCase):
+    """Tests for `wfc_reference` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
